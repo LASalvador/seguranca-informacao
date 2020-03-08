32d95e537a91e61b53fec9683890a35544c7ca0e
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-03-08 11:56:35 -0300

    adicionando router no client

---
+++ client/src/pages/ListaConversas.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ListaConversas extends Component {
+    render () {
+        return (
+            <h1>Page ListaConversas</h1>
+        )
+    }
+}
---
+++ client/src/route.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+// Components Imports
+import Chat from './pages/Chat';
+import ListaConversa from './pages/ListaConversas';
+import Home from './pages/Home';
+
+export default function Routes() {
+    return (
+        <Switch>
+            <Route path='/chat' exact component={Chat} />
+            <Route path='/list' exact component={ListaConversa} />
+            <Route path='/' exact component={Home} />
+        </Switch>
+    );
+}
