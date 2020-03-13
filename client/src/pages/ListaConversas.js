3de24c9da430dfc58d6f1a4a45d1627883787bc2
Mayara Brigida <mayara.brigida@trentim.com>
2020-03-12 21:05:24 -0300

    Lista Conversa

--- client/src/pages/ListaConversas.js
+++ client/src/pages/ListaConversas.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
-export default class ListaConversas extends Component {
-    render () {
-        return (
-            <h1>Page ListaConversas</h1>
-        )
-    }
+function Blog(props) {
+  const conteudo = props.cardList.map((cardList) =>
+    <div key={cardList.id}>
+      <br></br>
+      <h3>{cardList.title}</h3>
+      <p>{cardList.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {conteudo}
+    </div>
+  );
 }
+const cardList = [
+  {id: 1, name: 'Mayara', data: '12/02/2020'},
+  {id: 2, title: 'Installation', content: 'Mayara'}
+];
+
+function lista(props){
+  return(
+    <Blog cardList={cardList}/>
+  );
+}
+export default lista;
