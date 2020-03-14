1cee4acafbed66cadebaad9910ef95b1f7bd0daa
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-03-14 11:17:28 -0300

    adicionando chat card e atualizando chat page

---
+++ client/src/components/ChatCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginBottom: 10,
+    background: '#e4e4e4e4',
+  },
+  title: {
+    fontSize: 14,
+  },
+});
+
+export default function ChatCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {props.author}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {props.content}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import ChatCard from '../components/ChatCard';
 
 export default class Chat extends Component {
+    state = {
+        list: []
+    }
+
+    async componentDidMount (){
+        this.setState({list: [
+            {author: 'joao', content: 'maria'},
+            {author: 'joao2', content: 'maria2'},
+            {author: 'joao3', content: 'maria3'},
+            {author: 'joao4', content: 'maria4'},
+        ]})
+    }
+
     render () {
         return (
-            <h1>Page Chat</h1>
-        )
+            <section>
+                {
+                    this.state.list.map((msg) => {
+                       return <ChatCard 
+                            author={msg.author} 
+                            content={msg.content}
+                        />
+                    })
+                }
+
+            </section>
+        );
     }
 }
