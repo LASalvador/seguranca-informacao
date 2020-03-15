badf00f82a666bdbd8fac287f46ba94390c32757
Mayara Brigida <mayara.brigida@trentim.com>
2020-03-15 14:10:49 -0300

    Alteração ListaConversas

--- client/src/pages/ListaConversas.js
+++ client/src/pages/ListaConversas.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function Blog(props) {
+  const classes = useStyles();
   const conteudo = props.cardList.map((cardList) =>
-    <div key={cardList.id}>
-      <br></br>
-      <h3>{cardList.title}</h3>
-      <p>{cardList.content}</p>
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <div key={cardList.id}>
+          <h3>{cardList.title}</h3>
+          <p>{cardList.dia} - {cardList.hora}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
   return (
-    <div>
-      {conteudo}
-    </div>
+      <div>
+        {conteudo}
+      </div>
   );
 }
 const cardList = [
-  {id: 1, title: 'Mayara', content: '12/02/2020'},
-  {id: 2, title: 'Installation', content: 'Mayara'}
+  {id: 1, title: 'Mayara', dia: '12/02/2020', hora:'12h30'},
+  {id: 2, title: 'Lucas',  dia: '13/02/2020', hora:'15h30'}
 ];
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 14,
+  },
+});
 
 function lista(props){
   return(
