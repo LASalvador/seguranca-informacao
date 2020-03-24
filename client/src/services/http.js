88edf67d581548979e861042a2d2d10d38a2b619
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-03-24 19:36:36 -0300

    atualizando hhtp

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -1,13 +1,9 @@
 import axios from 'axios'
 
 const api = 'http://localhost:8020/'
-const token = storage.token.pegar()
 
 const http = axios.create({
-  baseURL: api,
-  headers: {
-    'x-access-token': token,
-  },
+  baseURL: api
 })
 
 export default function () {
