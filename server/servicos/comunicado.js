e609ee4912c42e789634865f2cc66f2eeeaf9ec6
PauloHenrique7010 <paulohenrique7010@gmail.com>
2020-03-26 17:15:14 -0300

    adicionado select.js para select generico

---
+++ server/servicos/comunicado.js
@@ -0,0 +1,24 @@
+const selectPromise = require('./select');
+
+
+async function selectComunicado(cod_comunicado) {
+  var resposta  = await selectPromise('SELECT r.cod_resposta, ' +
+                         'r.conteudo_resposta, ' +
+                         'r.autor_resposta, ' +
+                         'r.data_resposta_criado, ' +
+                         'r.data_resposta_atualizado, ' +
+                         'c.data_comunicado, ' +
+                         'c.responsavel_comunicado, ' +
+                         'c.email_comunicado, ' +
+                         'c.data_comunicado_criado, ' +
+                         'c.data_comunicado_atualizado ' +
+                  'FROM resposta r  ' +
+                  'LEFT JOIN comunicado c on c.cod_comunicado = r.cod_comunicado ' +
+                  'WHERE r.cod_comunicado = ' +cod_comunicado);
+
+    return resposta;
+  }
+
+module.exports = {
+    selectComunicado,
+};
