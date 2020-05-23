2561aa0f4c5077dfd3ef1547e450d78e5f622eba
lucas <lucas.lr184@gmail.com>
2020-05-23 18:18:55 -0300

    iniciando função para leitura de template

--- server/servicos/email.js
+++ server/servicos/email.js
@@ -1,22 +1,24 @@
 const nodemailer = require('nodemailer');
 
 var remetente = nodemailer.createTransport({
-    host:'smtp.gmail.com',
+    host: 'smtp.gmail.com',
     service: '',
     port: 465,
     secure: true,
-    auth:{
-        user:'fatec1b.adm@gmail.com',
+    auth: {
+        user: 'fatec1b.adm@gmail.com',
         pass: '1ADMfatec'
     }
 });
 
-function enviaEmail(templatePath, dest, content){
+function enviaEmail(templatePath, dest, subject, body) {
     var teste = 'titulo legal';
-    var subject = ejs.render('<h1><%= teste %></h1>');
+
     var emailSend = {
-        from:'fatec1b.adm@gmail.com',
+        from: 'fatec1b.adm@gmail.com',
         to: dest,
-        subject: subject
+        subject: subject,
+        body: body
     }
+    
 }
