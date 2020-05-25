3743c0f930ce443b7c78cfd8043189327aac2df7
Mayara Brigida <mayara.brigida@trentim.com>
2020-05-25 17:00:09 -0300

    Realização do login e alteração em  cadastro de comunicado

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
-import api from '../services/http'
+import api from '../services/http';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
@@ -18,8 +19,8 @@ const useStyles = makeStyles({
       width: '50vw'
     },
     titulo: {
-        fontSize:30, 
-      },
+      fontSize:30, 
+    },
     espacobtt: {
         marginTop:40,
         textAlign: "center",
@@ -28,8 +29,20 @@ const useStyles = makeStyles({
 });
 
 function Item(props){
+  const history = useHistory();
+
     const classes = useStyles();
-    const [age, setAge] = React.useState('');
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+     async function realizarLogin(){
+      try {
+        await api.post("login", {email: email, senha: senha})
+        history.push("/")
+      } catch (error) {
+        alert("Erro ao fazer login");
+      }
+    }
     
     return (
         <FormControl className={classes.root}>
@@ -37,18 +50,21 @@ function Item(props){
             <TextField 
               id="email" 
               label="E-mail" 
-              className={classes.espaco} 
+              className={classes.espaco}
+              onChange={evento => setEmail(evento.target.value)} 
             />
             <TextField 
               id="senha" 
               label="Senha" 
               type="password" 
-              className={classes.espaco} 
+              className={classes.espaco}
+              onChange={evento => setSenha(evento.target.value)}
             />
             <Button 
               variant="contained" 
               color="primary" 
               className={classes.espacobtt}
+              onClick={realizarLogin}
             >
               Entrar
             </Button>
