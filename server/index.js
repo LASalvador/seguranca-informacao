21639d94c9ee4bd33e3ac1444ccffc97120f8a5d
lucas <lucas.lr184@gmail.com>
2020-06-23 21:52:00 -0300

    criando serviço log

--- server/index.js
+++ server/index.js
@@ -7,13 +7,9 @@ const validacao = require("./routes/validacao");
 const resposta = require("./routes/resposta");
 const login = require("./routes/login");
 const dpo = require("./routes/dpo");
-<<<<<<< HEAD
 const cripto = require('./servicos/criptografia');
 const logao = require('./servicos/logger');
 
-=======
-const cripto = require("./servicos/criptografia");
->>>>>>> 9445c771248acf1045169817c82625b40e74ecaa
 
 app.use(express.json());
 
@@ -30,12 +26,9 @@ app.get('/', (req, res) => {
   res.send('Rota principal');
 });
 
-<<<<<<< HEAD
 
 logao.sendLog('info', 'TESTE LOGaO');
 
-=======
->>>>>>> 9445c771248acf1045169817c82625b40e74ecaa
 app.listen(8020, () =>{
   console.log("Servidor rodando na porta 8020!")
 });
