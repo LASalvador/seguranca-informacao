fa665ff89f27b4a63d00485fbd00ee1669cf3231
PauloHenrique7010 <paulohenrique7010@gmail.com>
2020-07-07 20:27:10 -0300

    Corrigido campo data na hora de cadastrar no banco

--- server/servicos/dpo.js
+++ server/servicos/dpo.js
@@ -1,6 +1,7 @@
 const selectPromise = require('./select');
 const sqlite3 = require('sqlite3').verbose();
 const serviceLog = require('../servicos/logger');
+const dataService = require('../servicos/data');
 
 const db = new sqlite3.Database('db/AppDB.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
@@ -9,8 +10,9 @@ const db = new sqlite3.Database('db/AppDB.db', sqlite3.OPEN_READWRITE, (err) => 
 });
 
 function createDPO(nome_dpo, email_dpo, senha, desc_dpo, telefone_dpo){
+  const data_atual = dataService();
   return db.run('INSERT INTO dpo (nome_dpo, email_dpo, senha, desc_dpo, telefone_dpo, data_criado) '+
-    'VALUES ("'+nome_dpo+'","'+email_dpo+'","'+senha+'","'+desc_dpo+'","'+telefone_dpo+'",DATETIME());',
+    'VALUES ("'+nome_dpo+'","'+email_dpo+'","'+senha+'","'+desc_dpo+'","'+telefone_dpo+'","'+data_atual+'");',
     function (err) {
       if (err) {
         return console.log(err.message);
@@ -53,13 +55,14 @@ async function selectDPO(cod_dpo) {
   }
 
   function updateDPO(cod_dpo, nome_dpo, email_dpo, senha, desc_dpo, telefone_dpo){
+    const data_atual = dataService();
     return db.run('UPDATE dpo SET '+
                   'nome_dpo = "'+nome_dpo+
                   '", email_dpo = "'+email_dpo+
                   '", senha = "'+senha+
                   '", desc_dpo = "'+desc_dpo+
                   '", telefone_dpo = "'+telefone_dpo+
-                  '", data_atualizado = DATETIME()'+
+                  '", data_atualizado = "'+data_atual+'"'+
                   ' where cod_dpo = "'+cod_dpo+'";',
    
               function (err) {
--- server/servicos/resposta.js
+++ server/servicos/resposta.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const dataService = require('../servicos/data');
 const db = new sqlite3.Database('db/AppDB.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
@@ -6,9 +7,10 @@ const db = new sqlite3.Database('db/AppDB.db', sqlite3.OPEN_READWRITE, (err) => 
 });
 
 function insertQuery(conteudo, autor, cod_comunicado) {
+    const data_atual = dataService();
 
     return db.run('INSERT INTO resposta (conteudo_resposta, autor_resposta, data_resposta, data_resposta_atualizado, cod_comunicado) ' +
-        'values ("' + conteudo + '","' + autor + '",DATETIME(),DATETIME(),' + cod_comunicado + ');',
+        'values ("' + conteudo + '","' + autor + '","'+data_atual+'","'+data_atual+'",' + cod_comunicado + ');',
         function (err) {
             if (err) {
                 return console.log(err.message);
@@ -17,7 +19,8 @@ function insertQuery(conteudo, autor, cod_comunicado) {
 }
 
 function updateComunicado(cod_comunicado){
-    return db.run('UPDATE comunicado SET data_comunicado_atualizado = DATETIME() WHERE cod_comunicado = '+cod_comunicado);
+    const data_atual = dataService();
+    return db.run('UPDATE comunicado SET data_comunicado_atualizado = "'+data_atual+'" WHERE cod_comunicado = '+cod_comunicado);
 }
 
 module.exports = {
